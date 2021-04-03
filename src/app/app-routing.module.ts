75ae42015ddd93b07dc00ee0123b0be8e8fb76d8
rutuja <rjangade54@gmail.com>
2021-04-03 20:46:54 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { from } from 'rxjs';
+import {InfoComponent } from './info/info.component'
+import {UserComponent } from './user/user.component'
+import { UserdetailsComponent} from './userdetails/userdetails.component'
+import {HomeComponent} from './home/home.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: "info",
+    component: InfoComponent
+  },
+
+  {
+    path: "user",
+    component: UserComponent
+  },
+
+  {
+    path: "userdetails",
+    component: UserdetailsComponent
+  },
+
+  {
+    path: "home",
+    component: HomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoComponent } from './info/info.component';
+import { UserComponent } from './user/user.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    UserComponent,
+    UserdetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
